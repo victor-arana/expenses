5733aef3c050e15675206b88a9a763b1ebe2dfc3
victor.arana <victor.arana@walmart.com>
2018-12-07 20:07:15 -0600

    First Maven Structure created

---
+++ src/main/java/expenses/App.java
@@ -0,0 +1,13 @@
+package expenses;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
